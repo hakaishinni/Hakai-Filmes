72595513b465f69df5b5ef4076ff306b7d16f990
hakaishinni <martinsjkj917@gmail.com>
2026-05-15 17:40:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -41,29 +41,25 @@ function toggleTheme() {
 function abrirPlayer(idFilme) {
     window.filmeAbertoID = idFilme;
     document.getElementById('videoModal').classList.add('active'); 
-    document.getElementById('playerContainer').innerHTML = `<iframe src="https://myembed.biz/filme/${idFilme}" width="100%" height="300" frameborder="0" allowfullscreen></iframe>`;
+    
+    // FILMES: Altura padrão, pois não tem menu de episódios
+    document.getElementById('playerContainer').innerHTML = `<iframe src="https://myembed.biz/filme/${idFilme}" width="100%" style="height: 400px;" frameborder="0" allowfullscreen></iframe>`;
+    
     document.getElementById('ratingTitle').innerText = "De 1 a 5, como avalia este filme?";
     resetarEstrelas();
     if(window.adicionarViewNoFirebase) window.adicionarViewNoFirebase(idFilme);
 }
 
-function abrirPlayerSerie(idSerie, temporada, episodio) {
+function abrirPlayerSerie(idSerie) {
     window.filmeAbertoID = idSerie;
     document.getElementById('videoModal').classList.add('active'); 
-    document.getElementById('playerContainer').innerHTML = `<iframe src="https://myembed.biz/serie/${idSerie}/${temporada}/${episodio}" width="100%" height="300" frameborder="0" allowfullscreen></iframe>`;
+    
+    // SÉRIES: Altura dinâmica! Vai ocupar 70% da altura da tela (70vh) com um mínimo de 500px para garantir muito espaço para a lista!
+    document.getElementById('playerContainer').innerHTML = `<iframe src="https://myembed.biz/serie/${idSerie}" width="100%" style="height: 70vh; min-height: 500px;" frameborder="0" allowfullscreen></iframe>`;
+    
     document.getElementById('ratingTitle').innerText = "De 1 a 5, como avalia esta série/anime?";
     resetarEstrelas();
     if(window.adicionarViewNoFirebase) window.adicionarViewNoFirebase(idSerie);
-}
-
-// NOVA FUNÇÃO DE TESTE: Link puro sem informar temporada e episódio
-function abrirPlayerGeral(id) {
-    window.filmeAbertoID = id;
-    document.getElementById('videoModal').classList.add('active'); 
-    document.getElementById('playerContainer').innerHTML = `<iframe src="https://myembed.biz/serie/${id}" width="100%" height="300" frameborder="0" allowfullscreen></iframe>`;
-    document.getElementById('ratingTitle').innerText = "De 1 a 5, como avalia este título?";
-    resetarEstrelas();
-    if(window.adicionarViewNoFirebase) window.adicionarViewNoFirebase(id);
 }
 
 function resetarEstrelas() {
